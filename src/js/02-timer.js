d2cd1696f4fa4c3b33695865db4411f937725ad4
SashaMaslak <owmaslak@gmail.com>
2022-06-17 09:27:42 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,15 +3,8 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-// all modules
-import Notiflix from 'notiflix';
-
 // one by one
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Block } from 'notiflix/build/notiflix-block-aio'
 
 refs = {
    startBtn: document.querySelector('[data-start]'),
